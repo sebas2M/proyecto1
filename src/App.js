703243e3b83f7ad634729eedff100f5898d80096
Mauricio Collazos <mauricio@lextegrity.com>
2022-11-04 19:29:23 -0500

    Adding timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Countdown from 'react-countdown';
+import {useRef} from 'react'
+// import useSound from 'use-sound';
 function App() {
+  // const [play] = useSound(boopSfx);
+  const countdown = useRef()
+
+  let coundownAPI = null;
+  
+  const startCountdown = () => {
+    console.log(coundownAPI)
+    if (coundownAPI){
+      coundownAPI.start()
+    }
+    
+  }
+
+  const stopCountdown = () => {
+    console.log(coundownAPI)
+    if (coundownAPI){
+      coundownAPI.stop()
+    }
+    
+  }
+
+  const onMount = () => {
+    console.log("On Mount")
+    console.log(countdown)
+    if (countdown.current)
+      coundownAPI = countdown.current.getApi()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Countdown ref={countdown} date={Date.now() + 10000}  autoStart={false} onMount={onMount}/>
+      <button onClick={startCountdown}>Start</button>
+      <button onClick={stopCountdown}>Stop</button>
     </div>
   );
 }
